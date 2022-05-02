6764c8c260e0e023db509eb95a0ef77fca41e11b
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-05-02 10:59:04 +0530

    Coding done by A again

--- testing-git/src/main/java/com/cts/TestingGitApplication.java
+++ testing-git/src/main/java/com/cts/TestingGitApplication.java
@@ -9,6 +9,8 @@ public class TestingGitApplication {
 	public static void main(String[] args) {
 		System.out.println("Coding done by developer A");
 		System.out.println("Coding done by developer B");
+		System.out.println("Coding done by developer A again");
+		System.out.println("Coding done by developer A once again");
 		SpringApplication.run(TestingGitApplication.class, args);
 	}
 
