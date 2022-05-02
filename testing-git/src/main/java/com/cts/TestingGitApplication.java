21c5376cdc75f0d9cba392ae52bc1c20e91f3337
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-05-02 10:48:08 +0530

    Coding by A complete

--- testing-git/src/main/java/com/cts/TestingGitApplication.java
+++ testing-git/src/main/java/com/cts/TestingGitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingGitApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Coding done by developer A");
 		SpringApplication.run(TestingGitApplication.class, args);
 	}
 
